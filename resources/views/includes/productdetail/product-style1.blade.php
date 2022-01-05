9369e290d605304c9a9b92a0e41caad56ba35609
Muniraj Rajbanshi <munirajrajbanshi@gmail.com>
2022-01-05 18:01:29 +0545

    product-detail page, related product

--- resources/views/includes/productdetail/product-style1.blade.php
+++ resources/views/includes/productdetail/product-style1.blade.php
@@ -1,17 +1,24 @@
-<!-- Breadcrumbs -->
-<section id="breadcrumbs" class="py-3">
-  <div class="container">
+<section id="breadcrumb_item" class="pb-0 breadcrumb mb-0">
+    <div class="container">
       <div class="row">
-          <div class="col-12">
-              <ul class="m-0 p-0 d-flex align-items-center text-white">
-                  <li class="font-weight-bold">Home</li><span class="mx-2"><i class="fa fa-angle-right" aria-hidden="true"></i></span>
-                  <li class="">Product Detail</li>
-              </ul>
-          </div>
-          <div class="col-12 mt-3">
-              <h2 class="text-white">Product Detail</h2>
-          </div>
+        <div class="col-md-12 m-auto">
+          <nav aria-label="breadcrumb">
+            <ol class="breadcrumb">
+              <li class="breadcrumb-item font-weight-bold">
+                <a href="{{ url('/') }}"
+                  ><span><i class="fa fa-home" aria-hidden="true"></i></span>
+                  HOME</a
+                >
+              </li>
+              <li
+                class="breadcrumb-item font-weight-bold"
+                aria-current="page"
+              >
+                <a href="javascript:void(0)" class="text-dark"> Product Details </a>
+              </li>
+            </ol>
+          </nav>
+        </div>
       </div>
-  </div>
-</section>
-<!-- Breadcrumbs Ends -->
+    </div>
+  </section>
