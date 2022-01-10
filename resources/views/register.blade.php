ce2ac117b35d139ecc9a23d270621192923e03db
Bipin Joshi <joshibipin2052@gmail.com>
2022-01-10 14:38:37 +0545

    minor changes:

--- resources/views/register.blade.php
+++ resources/views/register.blade.php
@@ -1,83 +1,145 @@
 @extends('layouts.master')
 @section('content')
-    <!-- Breadcrumbs -->
-    <!-- <section id="breadcrumbs" class="py-3">
+    <section id="register" class="py-5">
         <div class="container">
-            <div class="row">
-                <div class="col-12">
-                    <ul class="m-0 p-0 d-flex align-items-center text-white">
-                        <li class="font-weight-bold">Home</li><span class="mx-2"><i class="fa fa-angle-right" aria-hidden="true"></i></span>
-                        <li class="">About Us</li>
-                    </ul>
-                </div>
-                <div class="col-12 mt-3">
-                    <h2 class="text-white">About Us</h2>
-                </div>
-            </div>
-        </div>
-    </section> -->
-    <!-- Breadcrumbs Ends -->
-    <!-- register Us -->
-    <section id="login-register-wrapper" class="p-4">
-
-        <div class="login-register-form p-xl-5 p-lg-5 p-md-2 p-0">
-            <div class="row position-relative">
-                <div class="image w-100"><img src="frontend/assets/images/banner/1 (2).jpg" class="img-fluid"></div>
-                <div class="col-xl-4 col-lg-6 col-md-8 position-absolute mx-auto form-wrapper">
-                    <form class="p-2" id="registerForm">
-                        <div class="text-center">
-                            <h1 class="font-weight-bold my-xl-4 my-md-3 my-4">Register</h1>
-                            <div class="form-group position-relative mb-xl-4 mb-md-3 mb-2">
-                                <input type="text" class="form-control border-top-0 border-right-0 border-left-0 rounded-0 shadow-none bg-transparent" id="registerFirstName" placeholder="Firstname">
-                                <i class="fa fa-user-o" aria-hidden="true"></i>
-                            </div>
-                            <div class="form-group position-relative mb-xl-4 mb-md-3 mb-2">
-                                <input type="text" class="form-control border-top-0 border-right-0 border-left-0 rounded-0 shadow-none bg-transparent" id="registerLastName" placeholder="Lastname">
-                                <i class="fa fa-user-o" aria-hidden="true"></i>
-                            </div>
-                            <div class="form-group position-relative mb-xl-4 mb-md-3 mb-2">
-                                <input type="email" class="form-control border-top-0 border-right-0 border-left-0 rounded-0
-                                        shadow-none bg-transparent" id="registerEmail" placeholder="Email">
-                                <i class="fa fa-envelope-o" aria-hidden="true"></i>
-                            </div>
-                            <div class="form-group position-relative mb-xl-4 mb-md-3 mb-2">
-                                <input type="password" class="form-control border-top-0 border-right-0 border-left-0 rounded-0 shadow-none bg-transparent" id="registerPassword" placeholder="Password">
-                                <i class="fa fa-key" aria-hidden="true"></i>
-                            </div>
-                            <div class="form-group position-relative mb-xl-4 mb-md-3 mb-2">
-                                <input type="password" class="form-control border-top-0 border-right-0 border-left-0 rounded-0 shadow-none bg-transparent" id="registerConfirmPassword" placeholder="Re-type Password">
-                                <i class="fa fa-key" aria-hidden="true"></i>
-                            </div>
-
-                            <button type="button" class="btn btn-success btn-block shadow border-0 py-2 text-uppercase"  id="createAccount">
-                                Create an Account
-                            </button>
-
-                            <p class="text-center mt-4">
-                                Already have an account?
-                                <span>
-                                <a href="login.html">Login</a>
-                            </span>
-
-                            </p>
-                            <div class="row mb-4 px-3 justify-content-center align-items-center">
-                                <h6 class="mb-xl-0 mb-md-2 mb-2 mr-2">Sign in with</h6>
-                                <div class="social-media d-flex justify-content-center h-100">
-                                    <div class="google text-center mr-3">
-                                        <a href="{{ url('/api/client/customer_login/google') }}" class="fa fa-google auth-google" aria-hidden="true"></a>
-                                    </div>
-                                    <div class="facebook text-center mr-3">
-                                        <a class="fa fa-facebook" aria-hidden="true"></a>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
+          <div class="row">
+            <div class="col-lg-8 col-12 m-auto">
+              <div class="row no-gutters">
+                <div class="col-md-7 m-auto">
+                  <div class="contact-form bg_register m-auto p-5">
+                    <h1 class="font-weight-bold text-center text-white">
+                      REGISTER
+                    </h1>
+  
+                    <form action="login.html">
+                      <div class="form-group mb-4">
+                        <input
+                          type="text"
+                          class="
+                            form-control
+                            bg-transparent
+                            border-top-0 border-left-0 border-right-0
+                            rounded-0
+                            border_button_form
+                          "
+                          id="registerFirstName"
+                          placeholder="First Name"
+                        />
+                      </div>
+                      <div class="form-group mb-4">
+                        <input
+                          type="text"
+                          class="
+                            form-control
+                            bg-transparent
+                            border-top-0 border-left-0 border-right-0
+                            rounded-0
+                            border_button_form
+                          "
+                          id="registerLastName"
+                          placeholder="Last Name"
+                        />
+                      </div>
+                      <div class="form-group mb-4">
+                        <input
+                          type="email"
+                          class="
+                            form-control
+                            bg-transparent
+                            border-top-0 border-left-0 border-right-0
+                            rounded-0
+                            border_button_form
+                          "
+                          id="registerEmail"
+                          placeholder="Email Address"
+                        />
+                      </div>
+                      <div class="form-group mb-4">
+                        <input
+                          type="password"
+                          class="
+                            form-control
+                            bg-transparent
+                            border-top-0 border-left-0 border-right-0
+                            rounded-0
+                            border_button_form
+                          "
+                          id="registerPassword"
+                          placeholder="Password "
+                        />
+                      </div>
+                      <div class="form-group mb-4">
+                        <input
+                          type="password"
+                          class="
+                            form-control
+                            bg-transparent
+                            border-top-0 border-left-0 border-right-0
+                            rounded-0
+                            border_button_form
+                          "
+                          id="registerConfirmPassword"
+                          placeholder="Confirm Password"
+                        />
+                      </div>
+                      <div
+                        class="
+                          text-center
+                          btn_groups-single
+                          product_detail__bg
+                          m-auto
+                          w-50
+                        "
+                      >
+                        <button
+                          type="submit"
+                          class="btn product_detail__bg py-1 text-light"
+                          id="createAccount"
+                        >
+                          SIGN UP
+                        </button>
+                      </div>
                     </form>
+                    <h6 class="text-white font-weight-bold text-center mb-0 py-4">
+                      LOGIN WITH SOCIAL NETWORKING
+                    </h6>
+                    <div class="social-icon-footer ">
+                      <ul class="d-flex justify-content-around">
+                        <li class="facebook_bg register_button">
+                          <a href="" class="text-white"
+                            ><i class="fa fa-facebook" aria-hidden="true"></i
+                          ></a>
+                        </li>
+                        <li class="twitter_bg register_button">
+                          <a href="" class="text-white" ="" =""
+                            ><i class="fa fa-twitter" aria-hidden="true"></i
+                          ></a>
+                        </li>
+                        <li class="google_bg register_button">
+                          <a href="" class="text-white" ="" =""
+                            ><i class="fa fa-google-plus" aria-hidden="true"></i
+                          ></a>
+                        </li>
+                        <li class="linkedin_bg register_button">
+                          <a href="" class="text-white" ="" =""
+                            ><i class="fa fa-linkedin" aria-hidden="true"></i
+                          ></a>
+                        </li>
+                      </ul>
+                    </div>
+  
+                    <a
+                      class="text-white font-weight-bold text-right d-block w-100"
+                      href="{{route('login')}}"
+                      >Login</a
+                    >
+                  </div>
                 </div>
+              </div>
             </div>
-
+          </div>
         </div>
-    </section>
+      </section>
     <!-- register Us Ends -->
 @endsection
 @section('script')
@@ -241,6 +303,10 @@
                     localStorage.setItem("customerFname",data.data.first_name);
                     localStorage.setItem("customerLname",data.data.last_name);
                     localStorage.setItem("cartSession",'');
+                    
+                    localStorage.customerFname = data.data.first_name;
+                    localStorage.customerLname = data.data.last_name;
+
                     window.location.href = '/';
                 }
             },
